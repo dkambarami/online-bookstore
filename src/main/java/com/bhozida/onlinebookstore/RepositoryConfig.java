2b73a14c51e62d27f978f42634c3e8ea38eae24f
Denford Kambarami <dkambarami@gmail.com>
2020-05-06 12:37:07 +0000

    change config class to .java

--- src/main/java/com/bhozida/onlinebookstore/RepositoryConfig.java
+++ src/main/java/com/bhozida/onlinebookstore/RepositoryConfig.java
@@ -2,8 +2,7 @@ package com.bhozida.onlinebookstore;
 
 import com.bhozida.onlinebookstore.entity.*;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
